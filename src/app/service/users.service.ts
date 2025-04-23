924c598a8a7ffc7f03faa98b86215e77c26f11e5
thakurarun915 <rajpoot9897arun@gmail.com>
2025-04-23 17:43:15 +0530

    here doen error handling in http service

--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,18 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor(private http : HttpClient) { }
-getAllUsers(){
- return this.http.get("https://jsonplaceholder.typicode.com/users");
- 
- 
-}
+  constructor(private http: HttpClient) {}
 
+  getAllUsers(): Observable<any> {
+    return this.http.get("https://jsonplaceholder.typicode.com/userss").pipe(
+      catchError((error: any) => {
+        return throwError(() => error); // ✅ Modern throwError usage
+      })
+    );
+  }
 }
 
 
