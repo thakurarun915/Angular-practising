c1aef70c463c61a46ea6a051c923c74967d497de
thakurarun915 <rajpoot9897arun@gmail.com>
2025-04-24 23:09:15 +0530

    Practising subjects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { BindingComponent } from './binding/binding.component';
 import { UsersService } from './service/users.service';
 import { HttpClientModule } from '@angular/common/http';
-import { DashboardComponent } from './dashboard/dashboard.component'; // ✅ Correct import
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RxjsComponentComponent } from './rxjs-component/rxjs-component.component'; // ✅ Correct import
 
 @NgModule({
   declarations: [
     AppComponent,
     BindingComponent,
-    DashboardComponent
+    DashboardComponent,
+    RxjsComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rxjs-component/rxjs-component.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-rxjs-component',
+  standalone: false,
+  templateUrl: './rxjs-component.component.html',
+  styleUrl: './rxjs-component.component.css'
+})
+export class RxjsComponentComponent implements OnInit {
+ 
+  constructor(){}
+
+
+  ngOnInit() {
+
+    //Observables are unicast
+
+    const observable = new Observable(obj => obj.next(Math.random()));
+
+    //subscriber 1
+
+    observable.subscribe(d => console.log(d));
+
+
+    //subscribe 2
+    observable.subscribe(d => console.log(d));
+  }
+
+}
