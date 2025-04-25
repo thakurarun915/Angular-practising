d3732c47cb13d36881c054241b255da711441287
thakurarun915 <rajpoot9897arun@gmail.com>
2025-04-25 22:24:16 +0530

    learn behaviour and practise it

--- src/app/rxjs-component/rxjs-component.component.ts
+++ src/app/rxjs-component/rxjs-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import{ajax} from 'rxjs/ajax';
 
 @Component({
   selector: 'app-rxjs-component',
@@ -16,28 +17,51 @@ export class RxjsComponentComponent implements OnInit {
 
     //Observables are unicast
 
-    const observable = new Observable(obj => obj.next(Math.random()));
+    // const observable = new Observable(obj => obj.next(Math.random()));
 
-    //subscriber 1
+    // //subscriber 1
 
-    observable.subscribe(d => console.log(d));
+    // observable.subscribe(d => console.log(d));
 
 
-    //subscribe 2
-    observable.subscribe(d => console.log(d));
+    // //subscribe 2
+    // observable.subscribe(d => console.log(d));
+
+    // const subject = new Subject();
+
+
+    // //subscriber 1
+    // subject.subscribe(d => console.log(d));
+
+
+    // //subscriber 2
+    // subject.subscribe(d => console.log(d));
+
+    // subject.next(Math.random());
+
+    // const subject = new Subject();
+
+    // const data = ajax('https://jsonplaceholder.typicode.com/users');
+
+    // subject.subscribe(d => console.log(d));
+    // subject.subscribe(d => console.log(d));
+
+    // const result = data.subscribe(subject);
+     
 
     const subject = new Subject();
-
-
-    //subscriber 1
     subject.subscribe(d => console.log(d));
-
-
-    //subscriber 2
     subject.subscribe(d => console.log(d));
+    subject.next(2020);
 
-    subject.next(Math.random());
+    //behavior subject
+    const bsubject = new BehaviorSubject<number>(12);
+    bsubject.subscribe(d => console.log('BehaviorSubject Subscriber  ${d}'));
+
+    
 
   }
 
 }
+
+//https://jsonplaceholder.typicode.com/users
